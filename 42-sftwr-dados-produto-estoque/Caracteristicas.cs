47a3dcc836cf5cc5adf9d218ffde9f50c526a47a
Hhenrique-Gomes <henriquegomes99h@gmail.com>
2022-03-24 13:26:57 -0300

    1° versão estoque produto, cadastrar nome do produto e valor, logo em seguida mostrar valor total do produto, logo em seguida perguntar se deseja cadastrar mais produto

---
+++ 42-sftwr-dados-produto-estoque/Caracteristicas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_sftwr_dados_produto_estoque
+{
+    class Caracteristicas
+    {
+
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+        public string Linha;
+
+        public double ValorTotalProduto()
+        {
+
+            return Preco * Quantidade;
+
+        }
+
+        public void AddProduto (int quantidade)
+        {
+
+            Quantidade += quantidade;
+
+        }
+
+        public void Rtrr( int rtrr)
+        {
+
+            Quantidade -= rtrr;
+
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("{0}, R$ {1}, {2} unidades, Total: R$ {3} ", Nome, Preco.ToString("F2"), Quantidade, ValorTotalProduto());
+            Linha = Console.ReadLine();
+
+            return Linha;
+
+            /*
+            return Nome;  
+            + ", $ "
+            + Preco.ToString("F2")
+            + ", "
+            + Quantidade
+            + " unidadedes, Total: $ "
+            + ValorTotalProduto().ToString("F2");
+            */
+
+        }
+
+    }
+}
---
+++ 42-sftwr-dados-produto-estoque/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _42_sftwr_dados_produto_estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Caracteristicas j = new Caracteristicas();
+
+            Console.WriteLine("Entre com os dados do produto");
+            Console.WriteLine("Digite o nome do produto: ");
+            j.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a quantidade do produto: ");
+            j.Quantidade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o preço do produto: ");
+            j.Preco = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("{0}", j);
+
+            Console.Write("Deseja adicionar mais produtos?");
+            Console.WriteLine("Digite a quantidade a ser inserida ");
+            int qtd = Convert.ToInt32(Console.ReadLine());
+            j.AddProduto(qtd);
+
+            Console.WriteLine("{0}", j);
+
+            Console.Write("Deseja retirar algum produto?");
+            Console.WriteLine("Digite a quantidade a ser retirada");
+            qtd = Convert.ToInt32(Console.ReadLine());
+            j.Rtrr(qtd);
+
+            Console.WriteLine("{0}", j);
+         
+        }
+    }
+}
